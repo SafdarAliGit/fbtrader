ffd959533c215eac1ce0642bda42b2ec1801b135
SafdarAliGit <safdar211@gmail.com>
2023-06-12 17:50:58 +0500

    hlp changes applied

--- fbtrader/fbtrader/doctype/utils.py
+++ fbtrader/fbtrader/doctype/utils.py
@@ -11,6 +11,57 @@ from frappe.utils import get_url, quoted
 
 
 @frappe.whitelist()
+def fetch_recent_soled_items(**args):
+    item_code = args.get('item_code')
+    data = {}
+    data['sales_history'] = frappe.db.sql(
+        """
+        select 
+            `tabSales Invoice Item`.name, `tabSales Invoice Item`.parent,
+            `tabSales Invoice`.posting_date,`tabSales Invoice`.customer_name,
+            `tabSales Invoice Item`.item_code,`tabSales Invoice Item`.`item_name`,
+            `tabSales Invoice Item`.rate
+        from `tabSales Invoice`, `tabSales Invoice Item`
+        where `tabSales Invoice`.name = `tabSales Invoice Item`.parent
+            and `tabSales Invoice`.docstatus = 1 and `tabSales Invoice Item`.item_code = %s  order by `tabSales Invoice Item`.parent 
+        """,(item_code, ),
+        as_dict=1
+    )[:5]
+
+    data['purchase_history'] = frappe.db.sql(
+        """
+        select 
+            `tabPurchase Invoice Item`.name, `tabPurchase Invoice Item`.parent,
+            `tabPurchase Invoice`.posting_date,`tabPurchase Invoice`.supplier_name,
+            `tabPurchase Invoice Item`.item_code,`tabPurchase Invoice Item`.`item_name`,
+            `tabPurchase Invoice Item`.rate
+        from `tabPurchase Invoice`, `tabPurchase Invoice Item`
+        where `tabPurchase Invoice`.name = `tabPurchase Invoice Item`.parent
+            and `tabPurchase Invoice`.docstatus = 1 and `tabPurchase Invoice Item`.item_code = %s  order by `tabPurchase Invoice Item`.parent 
+        """, (item_code,),
+        as_dict=1
+    )[:5]
+    return data
+
+@frappe.whitelist()
+def fetch_recent_purchased_items(**args):
+    item_code = args.get('item_code')
+    data = frappe.db.sql(
+        """
+        select 
+            `tabPurchase Invoice Item`.name, `tabPurchase Invoice Item`.parent,
+            `tabPurchase Invoice`.posting_date,`tabPurchase Invoice`.supplier_name,
+            `tabPurchase Invoice Item`.item_code,`tabPurchase Invoice Item`.`item_name`,
+            `tabPurchase Invoice Item`.rate
+        from `tabPurchase Invoice`, `tabPurchase Invoice Item`
+        where `tabPurchase Invoice`.name = `tabPurchase Invoice Item`.parent
+            and `tabPurchase Invoice`.docstatus = 1 and `tabPurchase Invoice Item`.item_code = %s  order by `tabPurchase Invoice Item`.parent 
+        """, (item_code,),
+        as_dict=1
+    )[:5]
+    return data
+
+@frappe.whitelist()
 def fetch_child_records(**args):
     child_records = frappe.get_all("Receipt Form Item", filters={"payment_form_id": args.get("master_name")},
                                    fields=["name",
