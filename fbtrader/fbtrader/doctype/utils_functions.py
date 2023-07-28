745db611f06f7db2bfc78a74d453d83fec2d4337
SafdarAliGit <safdar211@gmail.com>
2023-07-28 15:01:48 +0500

    updated

--- fbtrader/fbtrader/doctype/utils_functions.py
+++ fbtrader/fbtrader/doctype/utils_functions.py
@@ -22,6 +22,7 @@ def get_purchase_related_jv(reference_name):
         LIMIT 1
     """
     jea = frappe.db.sql(query, as_dict=True)
+    frappe.throw(f"{jea}")
     je = frappe.get_doc('Journal Entry', jea.parent)
     if je:
         return je
